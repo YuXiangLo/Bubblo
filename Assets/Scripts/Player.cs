1c75aa1f5d9f71fc30a155536698b1860c2d74d9
YuXiangLo <linus13514@gmail.com>
2024-10-11 17:11:58 +0800

    Scene Transition, PlayerFineTune, CodeRefactor, RaycastDebugBox

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,137 +5,79 @@ using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // player move speed
-    public float MoveSpeed = 6f;
-
-    // The acceleration of the gravity
+public class Player : MonoBehaviour {
+    public float MoveSpeed = 10f;
     public float Gravity = -45f;
+    public float FloatingYSpeed = -2f;
+    public float JumpForce = 20f;
+    public float PlayerSize = 1f; 
+	public float FloatingRatio = 0.9995f;
+	public float TriggerDistance = 0.001f;
 
-    // floating speed
-    public float FloatingSpeed = -2f;
-
-    // the velocity going up when the player presses jump
-    public float JumpForce = 15f;
-
-    // player size
-    public float PlayerSize = 0.5f;
-
-	public float FloatingRatio = 0.8f;
-
-	public float TriggerDistance = 0.1f;
-
-    [SerializeField] float _currentBlood, _maxBlood = 10f;
-    public BloodBar _bloodBar;
-    public Vector2 _velocity = Vector2.zero;
     public bool _isGrounded = false;
     public bool _isFloating = false;
-    public Rigidbody2D _rigidbody2D;
-    public Camera _mainCamera;
+	public bool _canFloat = false;
+	public float FloatingXSpeed = 10f;
+    public Vector2 _velocity = Vector2.zero;
+    private Camera _mainCamera;
+    private Rigidbody2D _rigidbody2D;
     
-
-    // Start is called before the first frame update
-    private void Awake()
-    {
+    private void Awake() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
 		_rigidbody2D.freezeRotation = true;
-        _bloodBar = GetComponentInChildren<BloodBar>();
         _mainCamera = Camera.main;
     }
 
-    private void Start()
-    {
-        _bloodBar.UpdateBloodBar(_currentBlood, _maxBlood);
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
+    private void Update() {
         _isGrounded = _rigidbody2D.Raycast(Vector2.down, new Vector2(PlayerSize, PlayerSize), TriggerDistance);
-        _isFloating = Input.GetButton("Jump") && !_isGrounded;
-
+		FloatingMovementDetect();
         HorizontalMovementDetect();
-
         if (_isGrounded) 
-        {
             JumpMovementDetect();
-        }
-
         ApplyGravity();
         RestrictPlayerWithinCamera();
     }
 
-    private void FixedUpdate() 
-    {
+    private void FixedUpdate() {
         _rigidbody2D.MovePosition(_rigidbody2D.position + _velocity * Time.fixedDeltaTime);
+		FloatingXSpeed = _isGrounded ? MoveSpeed : FloatingXSpeed;
     }
 
-    private void HorizontalMovementDetect()
-    {
+	private void FloatingMovementDetect() {
+		if (_velocity.y < 0f && Input.GetButtonDown("Jump"))
+			_canFloat = true;
+		_isFloating = Input.GetButton("Jump")  && _canFloat;
+	}
+
+    private void HorizontalMovementDetect() {
         var horizontalInput = Input.GetAxis("Horizontal");
         float multiplier = _isFloating ? FloatingRatio : 1f;
-
-        _velocity.x = horizontalInput * MoveSpeed * multiplier;
+		FloatingXSpeed *= multiplier;
+        _velocity.x = horizontalInput * FloatingXSpeed;
     }
 
-    private void JumpMovementDetect()
-    {
-        // Keep the vertical speed to 0 when on the ground.
+    private void JumpMovementDetect() {
         _velocity.y = Mathf.Max(_velocity.y, 0f);
-
-        if (Input.GetButtonDown("Jump"))
-        {
+        if (Input.GetButtonDown("Jump")) {
             _velocity.y = JumpForce;
+			_canFloat = false;
         }
     }
 
-    private void ApplyGravity() 
-    {
-        if (_isFloating && _velocity.y < 0f)
-        {
-            _velocity.y = FloatingSpeed;
-        }
+    private void ApplyGravity() {
+        if (_isFloating && _velocity.y < FloatingYSpeed)
+            _velocity.y = FloatingYSpeed;
         else 
-        {
             _velocity.y += Gravity * Time.deltaTime;
-        }
     }
 
-    private void RestrictPlayerWithinCamera()
-    {
-        // Get the camera's boundaries in world space
+    private void RestrictPlayerWithinCamera() {
         float cameraHalfWidth = _mainCamera.orthographicSize * _mainCamera.aspect;
         float cameraLeftEdge = _mainCamera.transform.position.x - cameraHalfWidth;
         float cameraRightEdge = _mainCamera.transform.position.x + cameraHalfWidth;
 
-        // Get the player's current position
         Vector2 playerPosition = transform.position;
-
-        // Clamp the player's x position so they don't go outside the camera's view
         playerPosition.x = Mathf.Clamp(playerPosition.x, cameraLeftEdge + PlayerSize, cameraRightEdge - PlayerSize);
-
-        // Apply the clamped position back to the player
         transform.position = playerPosition;
     }
-
-    private void TakeDamage(float damageCount)
-    {
-        _currentBlood -= damageCount;
-        _bloodBar.UpdateBloodBar(_currentBlood, _maxBlood);
-        if (_currentBlood <= 0f)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-
-    }
-
-    // void OnCollisionEnter2D(Collision2D collision2D)
-    // {
-
-    // }
 }
--- Assets/Scripts/PlayerExtensions.cs
+++ Assets/Scripts/PlayerExtensions.cs
@@ -1,37 +1,33 @@
 using UnityEngine;
 
-public static class PlayerExtensions
-{
-    private static LayerMask _layerMask = LayerMask.GetMask("Ground");  // Assuming "Ground" is the layer for your ground
+public static class PlayerExtensions {
+    public static LayerMask _layerMask = LayerMask.GetMask("Ground");
+	public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction, Vector2 size, float distance) {
+		if (rigidbody.isKinematic)
+			return false;
 
-    // Custom BoxCast to check for ground or obstacles (only the bottom side)
-    public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction, Vector2 size, float distance)
-    {
-        if (rigidbody.isKinematic)
-        {
-            return false;
-        }
+		Vector2 castOrigin = rigidbody.position + new Vector2(0, -size.y * 0.5f);
+		Vector2 boxSize = new Vector2(size.x, 0.1f); // width: size.x, height: 0.1f
 
-        // Offset the origin of the BoxCast slightly downward to only check the bottom side (feet area)
-        Vector2 castOrigin = rigidbody.position + new Vector2(0, -size.y * 0.5f);  // Offset to bottom of the player
+		// Perform the BoxCast
+		RaycastHit2D hit = Physics2D.BoxCast(castOrigin, boxSize, 0f, direction, distance, _layerMask);
 
-        // Perform a BoxCast, reducing the height (size.y) to a small value
-        Vector2 boxSize = new Vector2(size.x, 0.1f);  // Width of the player's collider, but a small height for just the feet
+		// Visualize the box using Debug.DrawLine
+		Vector2 topLeft = castOrigin + new Vector2(-boxSize.x * 0.5f, boxSize.y * 0.5f);
+		Vector2 topRight = castOrigin + new Vector2(boxSize.x * 0.5f, boxSize.y * 0.5f);
+		Vector2 bottomLeft = castOrigin + new Vector2(-boxSize.x * 0.5f, -boxSize.y * 0.5f);
+		Vector2 bottomRight = castOrigin + new Vector2(boxSize.x * 0.5f, -boxSize.y * 0.5f);
 
-        RaycastHit2D hit = Physics2D.BoxCast(castOrigin, boxSize, 0f, direction, distance, _layerMask);
+		// Draw the lines representing the box
+		Debug.DrawLine(topLeft, topRight, Color.green, 5f);    // Top edge
+		Debug.DrawLine(topRight, bottomRight, Color.green, 5f); // Right edge
+		Debug.DrawLine(bottomRight, bottomLeft, Color.green, 5f); // Bottom edge
+		Debug.DrawLine(bottomLeft, topLeft, Color.green, 5f);    // Left edge
 
-        // Debugging visuals for BoxCast
-        Debug.DrawRay(castOrigin, direction * distance, Color.red, 5f);  // Visualize the cast direction
-        Debug.DrawLine(castOrigin - (Vector2.right * size.x * 0.5f), castOrigin + (Vector2.right * size.x * 0.5f), Color.green, 5f);  // Visualize the box's width
+		// Visualize the ray being cast
+		Debug.DrawRay(castOrigin, direction * distance, Color.red, 5f);
 
-        // Log the hit object for debugging
-        if (hit.collider != null)
-        {
-            Debug.Log("Hit object: " + hit.collider.gameObject.name + " on layer: " + LayerMask.LayerToName(hit.collider.gameObject.layer));
-        }
-
-        // Return true if the BoxCast hit something and it’s not the player itself
-        return hit.collider != null && hit.rigidbody != rigidbody;
-    }
+		return hit.collider != null && hit.rigidbody != rigidbody;
+	}
 }
 
