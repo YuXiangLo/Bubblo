d1e6c58eecad19920e743b2e2ba8ba89b987f9e6
lien0214 <ted20030214@gmail.com>
2024-10-21 19:27:32 +0800

    refactor: Bubble script and player attack

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -1,45 +1,50 @@
 using UnityEngine;
 
 public class Bubble : MonoBehaviour {
-    public float MaxBubbleSize = 3f;
-    public float MinBubbleSize = 0.3f;
+    public float MaxSize = 3f;
+    public float MinSize = 0.3f;
 
-    [SerializeField] private float MinBubbleSpeed = 4f;
-    [SerializeField] private float MaxBubbleSpeed = 10f;
+    [SerializeField] private float MinSpeed = 4f;
+    [SerializeField] private float MaxSpeed = 10f;
     [SerializeField] private float LifeTime = 5f;
     [SerializeField] private float MinDamage = 3f;
     [SerializeField] private float MaxDamage = 30f;
 
-	private bool IsRelease;
-	private float CurrentBubbleSize;
+    private Player Player;
+    private bool PlayerFacingRight => Player.IsFacingRight;
+    private float PlayerSize = 1f;
+
+    private bool IsCharging = true;
+	private bool IsRelease = false;
+    private float ChargingTime = 0f;
+    private const float MaxChargingTime = 2f;
+	private float CurrentSize => Mathf.Lerp(MinSize, MaxSize, ChargingTime / MaxChargingTime);
+    private float ReleaseSpeed => Mathf.Lerp(MinSpeed, MaxSpeed, ChargingTime / MaxChargingTime);
     private Rigidbody2D rb;
 
     private void Awake() {
+        Player = FindObjectOfType<Player>();
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    public void InitializeGrowing() {
-        transform.localScale = Vector3.one * MinBubbleSize;
+        transform.localScale = Vector3.one * MinSize;
         rb.velocity = Vector2.zero;
-		IsRelease = false;
+        UpdatePosition();
     }
 
-    public void UpdateBubbleSize(float holdTime, float maxHoldTime) {
-        CurrentBubbleSize = Mathf.Lerp(MinBubbleSize, MaxBubbleSize, holdTime / maxHoldTime);
-        transform.localScale = Vector3.one * CurrentBubbleSize;
+    private void Update() {
+        if (!IsRelease) {
+            if (IsCharging) {
+                ChargingTime += Time.deltaTime;
+                Debug.Log(ChargingTime);
+                UpdateSize();
+            }
+            UpdatePosition();
+        }
     }
 
-    public void ReleaseAndShoot(float holdTime, bool playerFacingRight) {
-        float bubbleSpeed = Mathf.Lerp(MaxBubbleSpeed, MinBubbleSpeed, holdTime / 2f);
-        rb.velocity = new(bubbleSpeed * (playerFacingRight ? 1 : -1), 0);
-        Destroy(gameObject, LifeTime);
-		IsRelease = true;
-    }
-
-	private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
 			Debug.Log("Collision detected with " + other.gameObject.name + " on layer " + other.gameObject.layer);
-			float damage = Mathf.Lerp(MinDamage, MaxDamage, CurrentBubbleSize / MaxBubbleSize);
+			float damage = Mathf.Lerp(MinDamage, MaxDamage, CurrentSize / MaxSize);
 			other.gameObject.GetComponent<IModifyHealth>().TakeDamage(damage);
 		}
 		if(!other.gameObject.CompareTag("Player") && !other.gameObject.CompareTag("Bubble")) {
@@ -54,5 +59,29 @@ public class Bubble : MonoBehaviour {
 			}
 		}
 	}
+
+    public void StopCharging() {
+        IsCharging = false;
+    }
+
+    public void Release() {
+        IsRelease = true;
+        rb.velocity = new(ReleaseSpeed * (PlayerFacingRight ? 1 : -1), 0);
+        Destroy(gameObject, LifeTime);
+    }
+
+    private void UpdateSize() {
+        transform.localScale = Vector3.one * CurrentSize;
+    }
+    private void UpdatePosition() {
+        transform.position = Player.transform.position + CalculateBubblePosition();
+    }
+
+	private Vector3 CalculateBubblePosition() {
+        float bubbleRadius = CurrentSize / 2f;
+        float xOffset = (PlayerSize / 2f + bubbleRadius) * (Player.IsFacingRight ? 1 : -1);
+        float yOffset = Mathf.Max(0f, bubbleRadius - PlayerSize / 2f + 0.05f);
+        return new Vector3(xOffset, yOffset, 0);
+    }
 }
 
--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -2,80 +2,63 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
     [SerializeField] private GameObject BubblePrefab;
-    [SerializeField] private float PlayerSize = 1f;
     [SerializeField] private float MaxMagicPoint = 10f;
 
     private float CurrentMagicPoint;
-    private bool IsButtonHeld = false;
-    private float ButtonHoldTime = 0f;
-    private float MaxBubbleSize;
-    private float MinBubbleSize;
-    private Player Player;
-    private GameObject currentBubble;
-    private Rigidbody2D PlayerRigidbody;
-    private const float MaxHoldTime = 2f; // Maximum time that affects bubble size and speed
+    private Bubble CurrentBubble;
+    private float HoldTime = 0f;
+    private const float MaxHoldTime = 2f;
 
     public float MagicPercentage => CurrentMagicPoint / MaxMagicPoint;
 
-    private void Awake() {
-        PlayerRigidbody = GetComponent<Rigidbody2D>();
-        Player = GetComponent<Player>();
-        Bubble bubbleScript = BubblePrefab.GetComponent<Bubble>();
-        MaxBubbleSize = bubbleScript.MaxBubbleSize;
-        MinBubbleSize = bubbleScript.MinBubbleSize;
+    private void Awake()
+    {
         CurrentMagicPoint = MaxMagicPoint;
+
+        CurrentBubble = null;
     }
 
-    public void HandleAttack() {
-        if (Input.GetButtonDown("Fire1")) {
-            ButtonHoldTime = 0f;
-            IsButtonHeld = true;
-            StartGrowingBubble();
-        }
-
-        if (Input.GetButton("Fire1") && IsButtonHeld && currentBubble != null && ButtonHoldTime < MaxHoldTime) {
-            if (CurrentMagicPoint > 0f) {
-                CurrentMagicPoint = Mathf.Clamp(CurrentMagicPoint - Time.deltaTime, 0f, MaxMagicPoint);
-                ButtonHoldTime += Time.deltaTime;
-            }
-
-            float sizeFactor = Mathf.Lerp(MinBubbleSize, MaxBubbleSize, ButtonHoldTime / MaxHoldTime);
-            Vector2 newBubblePosition = CalculateBubblePosition(sizeFactor / 2f);
-            currentBubble.transform.position = newBubblePosition;
-
-            if (CurrentMagicPoint > 0f)
-            {
-                Bubble bubbleScript = currentBubble.GetComponent<Bubble>();
-                bubbleScript.UpdateBubbleSize(ButtonHoldTime, MaxHoldTime);
+    public void HandleAttack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("MagicPercentage" + MagicPercentage);
+            InitialBubble();
+            if (MagicPercentage <= 0) {
+                StopChargingBubble();
+                ReleaseBubble();
             }
         }
 
-        if (Input.GetButtonUp("Fire1") && IsButtonHeld) {
-            IsButtonHeld = false;
-            ReleaseBubble();
-            ButtonHoldTime = 0f;
+        if (CurrentBubble != null)
+        {
+            if (CurrentMagicPoint > 0 && HoldTime < MaxHoldTime) {
+                CurrentMagicPoint -= Time.deltaTime;
+                HoldTime += Time.deltaTime;
+            }
+            else {
+                StopChargingBubble();
+            }
+        }
+        
+        if (Input.GetButtonUp("Fire1")) {
+            if (CurrentBubble != null) {
+                ReleaseBubble();
+            }
         }
     }
 
-    private void StartGrowingBubble() {
-        currentBubble = Instantiate(BubblePrefab, CalculateBubblePosition(MinBubbleSize / 2f), Quaternion.identity);
-        Bubble bubbleScript = currentBubble.GetComponent<Bubble>();
-        bubbleScript.InitializeGrowing();
+    private void InitialBubble() {
+        CurrentBubble = Instantiate(BubblePrefab).GetComponent<Bubble>();
+        HoldTime = 0f;
     }
-
+    
+    private void StopChargingBubble() {
+        CurrentBubble.StopCharging();
+    }
     private void ReleaseBubble() {
-        if (currentBubble != null) {
-            float clampedHoldTime = Mathf.Clamp(ButtonHoldTime, 0f, MaxHoldTime);
-            Bubble bubbleScript = currentBubble.GetComponent<Bubble>();
-            bubbleScript.ReleaseAndShoot(clampedHoldTime, Player.IsFacingRight);
-            currentBubble = null;
-        }
-    }
-
-    private Vector2 CalculateBubblePosition(float bubbleRadius) {
-        float xOffset = (PlayerSize / 2f + bubbleRadius) * (Player.IsFacingRight ? 1 : -1);
-        float yOffset = Mathf.Max(0f, bubbleRadius - PlayerSize / 2f + 0.05f);
-        return new Vector2(PlayerRigidbody.position.x + xOffset, PlayerRigidbody.position.y + yOffset);
+        CurrentBubble.Release();
+        CurrentBubble = null;
     }
 }
 
