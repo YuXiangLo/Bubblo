984bfce1dd5ede2906365814232f8ac16d86116f
YuXiangLo <linus13514@gmail.com>
2024-11-19 12:13:50 +0800

    feat: camera can turn into inspect mode

--- Assets/Scripts/SceneRelated/LevelData.cs
+++ Assets/Scripts/SceneRelated/LevelData.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class LevelData : MonoBehaviour
 {
     public float MinXAxisPosition = 0f;
-    public float MinYAxisPosition = 0f;
 	public float MaxXAxisPosition = 38f;
+    public float MinYAxisPosition = 0f;
+    public float MinInspectXAxisPosition = 0f;
+    public float MaxInspectXAxisPosition = 38f;
+    public float MinInspectYAxisPosition = 0f;
     public float StopFollowingXAxisYOffset = -60f;
 }
