e68ffd5dac7320223a15266e2464bfad72470624
lien0214 <ted20030214@gmail.com>
2024-12-03 21:17:06 +0800

    refactor: disable animator then bug vanished

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -81,6 +81,7 @@ public class Player : MonoBehaviour, IHealthPercentage, IMagicPercentage, IModif
 
     public void ChangeMovementState(IMovementState newState)
     {
+        Debug.Log($"{MovementState.GetType().Name} > {newState.GetType().Name}");
         MovementState = newState;
         MovementState.Enter();
     }
