f2ad60ec3a404a4c43abe1001c6a166eba91e07f
YuXiangLo <linus13514@gmail.com>
2024-12-13 18:21:43 +0800

    feat: bulletin new skin, dynamically hint words

--- Assets/Packages/Dialogue-System/DialogueTrigger.cs
+++ Assets/Packages/Dialogue-System/DialogueTrigger.cs
@@ -1,14 +1,41 @@
 using System;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour, IInteract {
+public class DialogueTrigger : MonoBehaviour, IInteract
+{
+    public Dialogue dialogue;
+    public Action Interact => InteractAction;
 
-	public Dialogue dialogue;
-	public Action Interact => InteractAction;
+    private Canvas childCanvas;
 
-	public void InteractAction ()
-	{
-		DialogueManager.Instance.StartDialogue(dialogue);
-	}
+    private void Awake()
+    {
+        // Get the child Canvas component
+        childCanvas = GetComponentInChildren<Canvas>(true); // Include inactive canvas
+        if (childCanvas == null)
+        {
+            Debug.LogWarning("No child Canvas found. Make sure to add a Canvas as a child of this GameObject.");
+        }
+    }
 
+    public void InteractAction()
+    {
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && childCanvas != null)
+        {
+            childCanvas.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && childCanvas != null)
+        {
+            childCanvas.gameObject.SetActive(false);
+        }
+    }
 }
